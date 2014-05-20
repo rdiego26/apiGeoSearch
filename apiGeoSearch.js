f6223f1244d990059915cb9505e3c3cd62bf9a4e
Diego Ramos <rdiego26@gmail.com>
2014-05-20 14:53:49 -0300

    OPTIME code and ADD default route

--- apiGeoSearch.js
+++ apiGeoSearch.js
@@ -12,6 +12,7 @@ var express  = require('express'),
 
 var constants = require('./utils/Constants');
 var geoSearchController = require('./controller/geoSearch');
+var _result = {status:{}, date:new Date(), data:[]};
 
 /* App Configuration */
 var app = express();
@@ -20,11 +21,12 @@ app.set('title', 'apiGeoSearch');
 
 
 /* Include CORS and JSON Type on ALL res's  */
-app.all('/*', function(req, res, next) {
+app.all('*', function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
     res.header('Content-type', constants.header.json);
+    res.removeHeader("X-Powered-By"); //Remove header for safety reasons
     next();
 });
 
@@ -35,6 +37,13 @@ app.get('/:address', function(req, res) {
 
 app.get('/:lat/:lng', function(req, res) {
     geoSearchController.searchByLatLng(req, res, geoSearchController.setData);
+});
+
+//DEFAULT ROUTE
+app.get( '*' , function(req, res) {
+    res.writeHead(constants.http.notFound, constants.header.json);
+    _result.status = constants.http.notFound;
+    res.end(JSON.stringify(_result));
 });
 
 http.createServer(app).listen(app.get('port'), function() {
